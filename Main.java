f996bfb8d99ac54c1299735d341c5d42e9b39d0f
avelez61 <velezaj13@protonmail.com>
2026-06-14 23:35:47 -0400

    Adds structure for main loop

--- Main.java
+++ Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("CLI Java Music App");
+		
+		boolean running = true;
+		while (running) {
+			System.out.print("Enter a command: ");
+			String input = scanner.nextLine().toUpperCase();
+			
+			if (input.equals("EXIT")) {
+				running = false;
+			}
+		}
+		
+		scanner.close();
 	}
 }
